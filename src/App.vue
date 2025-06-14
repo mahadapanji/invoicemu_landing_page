1a88522ab14f9cc1974e7f0516db76358f9c9bd4
Mahada Panji Anggadhita <mahada.panji@ymail.com>
2025-06-14 09:24:41 +0700

    add features
    add features

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 import Navbar from "./components/Navbar.vue";
 import Hero from "./components/Hero.vue";
 // import Benefits from "./components/Benefits.vue";
-// import Features from "./components/Features.vue";
+import Features from "./components/Features.vue";
 // import Services from "./components/Services.vue";
 // import HowItWorks from "./components/HowItWorks.vue";
 // import Sponsors from "./components/Sponsors.vue";
